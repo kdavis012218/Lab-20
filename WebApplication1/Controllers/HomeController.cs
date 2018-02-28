b2a39a5f7eef7de7a3855d5187d8b247454bb644
kdavis012218 <35358468+kdavis012218@users.noreply.github.com>
2018-02-27 20:42:03 -0500

    cup of life coffee shop

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Complete(Register r)
+        {
+            ViewBag.Message = "Registration Complete.";/*ViewBag simply shows what you ask it*/
+            ViewBag.Name = r.Firstname;
+            ViewBag.Name = r.lastname;
+            return View();/*this code section takes first/last name and adds it to the welcome message*/
+
+        }
+        public ActionResult StartPage() => View();
+    }
+}
+
+
+
+
+
+
+
+
+
+
---
+++ WebApplication1/Models/Register.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Register
+    {/*string name references for form info*/
+        public string Firstname { get; set; }
+        public string lastname { get; set; }
+        public string Password { get; set; }
+        public string gender { get; set; }
+
+    }
+}
